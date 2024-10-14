198c227b84102d2e32500930fda003f308249d28
Jinendran10 <jinendranjinu10@gmail.com>
2024-10-14 11:15:18 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Employee
+{
+	public static void main(String args[])
+	{
+		class Employee extends Employee_name
+		{
+			int n;
+			System.out.println("Enter the no.of Employees:");
+			Vector <String> col=new Vector<>():
+			n=s.nextInt();
+			col=print_name(n);
+		}	
+	}
+}
+class Employee_name
+{
+	 void print_name(int n);
+	 {
+	 	Vector <String> name=new Vector<>();
+	 	Scanner s=new Scanner();
+	 	int i;
+	 	for(i=0;i<n;i++)
+	 	{
+	 		System.out.println("Enter the Name of Employee "+i+" :");
+	 		name.add(i,s.nextLine());
+	 	}
+	 	for (i=0;i<n;i++)
+	 	{
+	 		return name;
+	 	}
+	 }
+}
+	 
+	 
+	 
+	 
+}
+
+	  
