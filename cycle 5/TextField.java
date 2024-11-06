1ac1d2a03b5bfa96a7352d27ec24c96c53357cf7
Jinendran10 <jinendranjinu10@gmail.com>
2024-11-07 00:00:28 +0530

    Create TextField.java

---
+++ cycle 5/TextField.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class test implements ActionListener{
+	JLabel l;
+	JTextField t;
+	JButton b;
+	test(){
+		JFrame f=new JFrame("TextField");
+		l=new JLabel("Nothing entered");
+		t=new JTextField(16);
+		b=new JButton("Submit");
+		JPanel p=new JPanel(new GridLayout(3,1));
+		p.add(l);
+		p.add(t);
+		p.add(b);
+		b.addActionListener(this);
+		f.add(p);
+		f.setSize(300,300);
+		f.show();
+	}
+	public void actionPerformed(ActionEvent e){
+		String s=e.getActionCommand();
+		if(s.equals("Submit")){
+			l.setText(t.getText());
+			t.setText("");
+		}
+	
+	public static void main(String args[]){
+		test t=new test();
+	}
+}
